cf0e924fcfda15e9df4d68b9d375eefa4d550d0a
l1va <lev_matematik@tut.by>
2020-12-02 18:27:13 +0300

    encoders

--- hw/encoders_from_arduino.go
+++ hw/encoders_from_arduino.go
@@ -2,29 +2,57 @@ package main
 
 import (
 	"fmt"
+	"github.com/bugst/go-serial"
 	"github.com/eranet/rhombus/rhomgo"
+	"log"
+	"strconv"
+	"strings"
 )
 
-type Position struct {
-	Value float64
+type Encoders struct {
+	Pos1 float64
+	Vel1 float64
+	Pos2 float64
+	Vel2 float64
 }
 
 func main() {
 	c := rhomgo.LocalJSONConnection()
 	defer c.Close()
 
-	c.Subscribe("/current", func(p *Position) {
-		fmt.Printf("Received a position: %+v\n", p)
-	})
+	mode := &serial.Mode{
+		BaudRate: 115200,
+	}
+	port, err := serial.Open("", mode) //TODO: pass value
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	rate := rhomgo.NewRate(1)
-	for i := 1.0; i > -1; i -= 0.001 {
-		err := c.Publish("/pendabot/shoulder_torque_controller/command", Position{Value: i})
+	buff := make([]byte, 100)
+	for {
+		// Reads up to 100 bytes
+		n, err := port.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if n == 0 {
+			fmt.Println("\nEOF")
+			break
+		}
+
+		data := string(buff[:n])
+		data = strings.TrimSpace(data)
+		fmt.Printf("%s", data)
+		vals := strings.Split(data, " ")
+
+		err = c.Publish("/encoders", Encoders{Pos1: toF(vals[0]), Vel1: toF(vals[1]), Pos2: toF(vals[2]), Vel2: toF(vals[3])})
 		if err != nil {
 			println("error pub:", err)
 		}
-		rate.Sleep()
 	}
 
 }
-
+func toF(s string) float64 {
+	res, _ := strconv.ParseFloat(s, 64)
+	return res
+}
--- launch_world.sh
+++ launch_world.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ./server/gnatsd  &
-python3 hw/send_command.py
+python3 hw/send_command.py &
+go run hw/encoders_from_arduino.go
